725b3271d25c7fd7ed9006b9ff5c7a4347381ffd
pmsu2007 <pmsu2007@naver.com>
2025-03-20 20:08:50 +0900

    feat : 사이드바와 헤더 컴포넌트 구현

--- src/App.jsx
+++ src/App.jsx
@@ -12,23 +12,29 @@ import MacdGuide from './pages/guides/MacdGuide';
 import LongShortGuide from './pages/guides/LongShortGuide';
 import KimchiPremiumGuide from './pages/guides/KimchiPremiumGuide';
 import WhaleTransactionsGuide from './pages/guides/WhaleTransactionsGuide';
+import AlarmPage from "./pages/alarm/AlarmPage.jsx";
+import Header from "./components/Header.jsx";
 
 function App() {
   return (
     <Router>
-      <div className="w-screen h-screen overflow-hidden bg-[#0E106C]">
+      <Header/>
+      <div className="w-screen h-screen overflow-hidden bg-gradient-to-br from-[#0A1184] via-[#341684] to-[#0F5BAF]">
         <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/discord" element={<Discord />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          
+          <Route path="/" element={<Login/>}/>
+          <Route path="/discord" element={<Discord/>}/>
+          <Route path="/dashboard" element={<Dashboard/>}/>
+
           {/* 가이드 라우트 */}
-          <Route path="/guide/fear-greed" element={<FearGreedGuide />} />
-          <Route path="/guide/rsi" element={<RsiGuide />} />
-          <Route path="/guide/macd" element={<MacdGuide />} />
-          <Route path="/guide/long-short" element={<LongShortGuide />} />
-          <Route path="/guide/kimchi-premium" element={<KimchiPremiumGuide />} />
-          <Route path="/guide/whale-transactions" element={<WhaleTransactionsGuide />} />
+          <Route path="/guide/fear-greed" element={<FearGreedGuide/>}/>
+          <Route path="/guide/rsi" element={<RsiGuide/>}/>
+          <Route path="/guide/macd" element={<MacdGuide/>}/>
+          <Route path="/guide/long-short" element={<LongShortGuide/>}/>
+          <Route path="/guide/kimchi-premium" element={<KimchiPremiumGuide/>}/>
+          <Route path="/guide/whale-transactions" element={<WhaleTransactionsGuide/>}/>
+
+          {/* 알람 */}
+          <Route path="/alarm" element={<AlarmPage/>}/>
         </Routes>
       </div>
     </Router>
