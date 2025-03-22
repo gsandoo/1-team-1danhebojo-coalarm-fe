8bed449aa858ea3b1b58ef6c7736fe52267402c0
Dwisgolmog <rlaehdgus134@naver.com>
2025-03-22 16:58:33 +0900

    fix: StricMode 복구!

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import store from './redux/store'
@@ -5,9 +6,9 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <>
+  <StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </>
+  </StrictMode>
 )
